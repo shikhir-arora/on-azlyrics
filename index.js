88cb2393170f6170d7ed636b41c5fd0ed1490def
Shikhir Arora <shikhir@thesarogroup.com>
2017-08-06 12:47:16 -0400

    Take 2 :thumbsup:

--- index.js
+++ index.js
@@ -5,56 +5,57 @@ var pattern = /<a href="(.*)" target="_blank"><b>(.*)<\/b><\/a>  by <b>(.*)<\/b>
 var lpattern = /Sorry about that. -->([\s\S]*)<!-- MxM banner -->/g
 
 async function Request(song, page) {
-	try {
-		var url = 'http://search.azlyrics.com/search.php?q=' + song + '&w=songs&p=' + page
-		var html = await request(url)
-		return html.body
-	}
-	catch (e) {
-		console.log(e)
-		process.exit(1)
-	}
+    try {
+        var url = 'http://search.azlyrics.com/search.php?q=' + song + '&w=songs&p=' + page
+        var html = await request(url)
+        return html.body
+    } catch (e) {
+        console.log(e)
+        process.exit(1)
+    }
 }
 
 async function GetLyric(url) {
-	try {
-		var html = await request(url)
-		var raw = lpattern.exec(html.body)
-		var res = raw[1].replace(/\<br\>/g," ");
-        res = res.replace(/<i>\W*(\w+).+(\d+).*<\/i>/g, "\1 \2");
-		res = res.replace(/(?=)(\&quot\;)/g, "\"");
-		res = res.replace(/\<\/div\>/g, " ");
-		console.log(res)
-	}
-	catch (e) {
-		console.log(e)
-		process.exit(1)
-	}
+    try {
+        var html = await request(url)
+        var raw = lpattern.exec(html.body)
+        var res = raw[1].replace(/\<br\>/g, " ");
+        res = res.replace(/<i?>|.\/|\i\>/g, "");
+        res = res.replace(/(?=)(\&quot\;)/g, "\"");
+        res = res.replace(/div\>/g, "");
+        console.log(res)
+    } catch (e) {
+        console.log(e)
+        process.exit(1)
+    }
 }
 
 async function GetSongList(song, page) {
-	try {
-		var html = await Request(song, page)
-		var songs = []
-		var song
-		while (song = pattern.exec(html))
-			songs.push({url : song[1], name : song[2], singer : song[3]})
-		for (var i=0; i < songs.length; i++)
-			console.log((i+1).toString() + ' - ' + songs[i].name + ' - ' + songs[i].singer)
+    try {
+        var html = await Request(song, page)
+        var songs = []
+        var song
+        while (song = pattern.exec(html))
+            songs.push({
+                url: song[1],
+                name: song[2],
+                singer: song[3]
+            })
+        for (var i = 0; i < songs.length; i++)
+            console.log((i + 1).toString() + ' - ' + songs[i].name + ' - ' + songs[i].singer)
 
-		prompt.get(['number'], (err, result) => {
-			result.number--
-			GetLyric(songs[result.number].url)
-		})
-	}
-	catch (e) {
-		console.log(e)
-		process.exit(1)
-	}
+        prompt.get(['number'], (err, result) => {
+            result.number--
+                GetLyric(songs[result.number].url)
+        })
+    } catch (e) {
+        console.log(e)
+        process.exit(1)
+    }
 }
 
 function clear() {
-	console.log('\033[2J')
+    console.log('\033[2J')
 }
 
 var song, page
@@ -62,5 +63,5 @@ var song, page
 prompt.start()
 
 prompt.get(['song', 'page'], (err, result) => {
-	GetSongList(result.song, result.page)
+    GetSongList(result.song, result.page)
 })
--- index3.js
+++ index3.js
@@ -4,53 +4,54 @@ var pattern = /<a href="(.*)" target="_blank"><b>(.*)<\/b><\/a>  by <b>(.*)<\/b>
 var lpattern = /Sorry about that. -->([\s\S]*)<!-- MxM banner -->/g
 
 async function Request(song) {
-        try {
-                let page = 1
-                var url = 'http://search.azlyrics.com/search.php?q=' + song + '&w=songs&p=' + page
-                var html = await request(url)
-                return html.body
-        }
-        catch (e) {
-                console.log(e)
-                process.exit(1)
-        }
+    try {
+        let page = 1
+        var url = 'http://search.azlyrics.com/search.php?q=' + song + '&w=songs&p=' + page
+        var html = await request(url)
+        return html.body
+    } catch (e) {
+        console.log(e)
+        process.exit(1)
+    }
 }
 
 async function GetLyric(url) {
-        try {
-                var html = await request(url)
-                var raw = lpattern.exec(html.body)
-                var res = raw[1].replace(/\<br\>/g," ");
-                res = res.replace(/<i>\W*(\w+).+(\d+).*<\/i>/g, "\1 \2");
-                res = res.replace(/(?=)(\&quot\;)/g, "\"");
-                res = res.replace(/\<\/div\>/g, "");
-                return res
-        }
-        catch (e) {
-                console.log(e)
-                process.exit(1)
-        }
+    try {
+        var html = await request(url)
+        var raw = lpattern.exec(html.body)
+        var res = raw[1].replace(/\<br\>/g, " ");
+        res = res.replace(/<i?>|.\/|\i\>/g, "");
+        res = res.replace(/(?=)(\&quot\;)/g, "\"");
+        res = res.replace(/div\>/g, "");
+        return res
+    } catch (e) {
+        console.log(e)
+        process.exit(1)
+    }
 }
 
 async function GetSongList(song) {
-        try {
-                var html = await Request(song)
-                var songs = []
-                var song
-                while (song = pattern.exec(html))
-                        songs.push({url : song[1], name : song[2], singer : song[3]})
-                console.log(songs[0].name + ' - ' + songs[0].singer)
-                var result = await GetLyric(songs[0].url)
-                console.log(result)
-        }
-        catch (e) {
-                console.log(e)
-                process.exit(1)
-        }
+    try {
+        var html = await Request(song)
+        var songs = []
+        var song
+        while (song = pattern.exec(html))
+            songs.push({
+                url: song[1],
+                name: song[2],
+                singer: song[3]
+            })
+        console.log(songs[0].name + ' - ' + songs[0].singer)
+        var result = await GetLyric(songs[0].url)
+        console.log(result)
+    } catch (e) {
+        console.log(e)
+        process.exit(1)
+    }
 }
 
 function clear() {
-        console.log('\033[2J')
+    console.log('\033[2J')
 }
 
 
